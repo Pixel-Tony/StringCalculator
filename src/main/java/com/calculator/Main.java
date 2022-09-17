bd8368b365e7a111980a4ba7a0e0a19a29ff38a3
Pixel-Tony <ant.peshkoff9@gmail.com>
2022-09-17 15:31:24 +0200

    ---

--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -1,4 +1,4 @@
-package org.calculator;
+package com.calculator;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,8 +7,10 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) {
+
         var calc = new StringCalculator();
         var br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("To quit main, enter 'q'");
         while (true) {
             try {
                 String input = br.readLine().replace("\\n", "\n");
